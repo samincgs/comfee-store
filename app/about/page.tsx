686b074d4ad4ce5aad726ed028346d4300f3fc70
samin <samincgs@gmail.com>
2024-06-13 23:08:13 -0400

    created the product model in prisma

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,19 +1,7 @@
-import { db } from '@/utils/db';
-
-const AboutPage = async () => {
-  const posts = await db.post.create({
-    data: {
-      name: 'Hello World',
-    },
-  });
-
-  const allPost = await db.post.findMany();
-
+const AboutPage = () => {
   return (
     <div>
-      {allPost.map((post) => {
-        return <div key={post.id}>{post.name}</div>;
-      })}
+      <h1>About Page</h1>
     </div>
   );
 };
