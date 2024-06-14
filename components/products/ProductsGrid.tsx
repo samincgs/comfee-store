07c1ab573588cb41bb2c4907f14f923a6dc8c0fb
samin <samincgs@gmail.com>
2024-06-14 01:56:48 -0400

    refactored Featured products

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -1,6 +1,10 @@
 import { Product } from '@prisma/client';
 import { formatCurrency } from '@/utils/format';
-import ProductItem from './ProductItem';
+import { Card, CardContent } from '../ui/card';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Button } from '../ui/button';
+import FavoriteToggleButton from './FavoriteToggleButton';
 
 type ProductsGridProps = {
   products: Product[];
@@ -13,16 +17,43 @@ const ProductsGrid = ({ products }: ProductsGridProps) => {
         const { name, price, image, company } = product;
         const productId = product.id;
         const formattedPrice = formatCurrency(price);
-
-        const productProps = {
-          name,
-          price: formattedPrice,
-          company,
-          image,
-          productId,
-        };
-
-        return <ProductItem key={productId} productProps={productProps} />;
+        return (
+          <div key={productId} className='group relative'>
+            <Card className='shadow-md group-hover:shadow-xl transition-shadow duration-300'>
+              <CardContent className='p-0'>
+                <Link href={`/products/${productId}`}>
+                  <div className='relative h-52 rounded-md overflow-hidden '>
+                    <Image
+                      src={image}
+                      alt={name}
+                      fill
+                      priority
+                      sizes='100vw'
+                      className='object-cover rounded transform group-hover:scale-110 transition-transform'
+                    />
+                  </div>
+                  <div className='flex items-center justify-between p-4 pt-6'>
+                    <div className='flex flex-col gap-1'>
+                      <h1 className='font-bold '>{name}</h1>
+                      <p className='text-muted-foreground text-xs font-medium'>
+                        {company}
+                      </p>
+                    </div>
+                    <Button
+                      size={'sm'}
+                      className='text-sm -mt-2 border p-2 rounded-lg text-white shadow-sm hover:bg-primary'
+                    >
+                      {formattedPrice}
+                    </Button>
+                  </div>
+                </Link>
+              </CardContent>
+            </Card>
+            <div className='absolute top-2 right-3'>
+              <FavoriteToggleButton productId={productId} />
+            </div>
+          </div>
+        );
       })}
     </div>
   );
