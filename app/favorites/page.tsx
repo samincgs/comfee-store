4c6ed78e319151d864533a585f67a59b36a62eb7
samin <samincgs@gmail.com>
2024-06-24 00:57:52 -0400

    added all favorites to the favorites page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,4 +1,19 @@
-const FavoritesPage = () => {
-  return <div>favorites page</div>;
+import Heading from '@/components/global/Heading';
+import ProductsGrid from '@/components/products/ProductsGrid';
+import { fetchUserFavorites } from '@/utils/actions';
+
+const FavoritesPage = async () => {
+  const favorites = await fetchUserFavorites();
+
+  if (favorites.length === 0) {
+    return <Heading text='No Favorites Yet.' />;
+  }
+
+  return (
+    <>
+      <Heading text='Your Favorites' />
+      <ProductsGrid products={favorites.map((fav) => fav.product)} />
+    </>
+  );
 };
 export default FavoritesPage;
--- utils/actions.ts
+++ utils/actions.ts
@@ -270,3 +270,18 @@ export const toggleFavoriteAction = async (prevState: {
     return renderError(error);
   }
 };
+
+export const fetchUserFavorites = async () => {
+  const userId = await getAuthUser();
+
+  const favorites = await db.favorite.findMany({
+    where: {
+      clerkId: userId,
+    },
+    include: {
+      product: true,
+    },
+  });
+
+  return favorites;
+};
